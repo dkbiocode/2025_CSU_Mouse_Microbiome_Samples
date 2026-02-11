b8a19d8ec59780e2b0c28b20d48cd3332c455d89
David King <davidcking.inbox@gmail.com>
2026-02-11 13:52:21 -0700

    modified 01-02 files

--- 01.sh
+++ 01.sh
@@ -1,22 +1,20 @@
-#!/bin/bash
-
-#SBATCH --job-name=01.demultiplex
+#!/usr/bin/env bash
+#SBATCH --job-name=01.demultiplex-3
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks=2
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
-#SBATCH --time=24:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=e.s.esdaile@colostate.edu
-#SBATCH --output=01.demultiplex_%j.out
-
-module load anaconda
+#SBATCH --time=4:00:00
+#SBATCH --output=%x_%j.out
+module purge
+module load miniforge
 
 conda activate qiime2-amplicon-2024.10
 
 snakemake \
-        -c 4 \
+        -c $SLURM_NTASKS \
         -s 01.demultiplex \
         --verbose \
-        --printshellcmds
+        --printshellcmds \
+        demux_summarize_filtered
 
--- 02.sh
+++ 02.sh
@@ -2,20 +2,20 @@
 
 #SBATCH --job-name=02.denoise
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks=6
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
-#SBATCH --time=24:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=e.s.esdaile@colostate.edu
-#SBATCH --output=02.denoise_%j.out
+#SBATCH --time=02:00:00
+#SBATCH --output=%x_%j.out
 
-module load anaconda
+export TMPDIR=$SLURM_SCRATCH
+
+module load miniforge
 
 conda activate qiime2-amplicon-2024.10
 
 snakemake \
-        -c 4 \
+        -c $SLURM_NTASKS \
         -s 02.denoise \
         --verbose \
         --printshellcmds
