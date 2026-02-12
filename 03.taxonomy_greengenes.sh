e87679114a54ed128e1fd82758fdf5f1038987f7
David King <davidcking.inbox@gmail.com>
2026-02-11 19:41:38 -0700

    modifications for steps 3 and 4

--- 03.taxonomy_greengenes.sh
+++ 03.taxonomy_greengenes.sh
@@ -2,13 +2,11 @@
 
 #SBATCH --job-name=03.taxonomy_greengenes
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks=12
 #SBATCH --partition=amilan
-#SBATCH --time=24:00:00
+#SBATCH --time=2:00:00
 #SBATCH --qos=normal
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=e.s.esdaile@colostate.edu
-#SBATCH --output=03.taxonomy_greengenes_%j.out
+#SBATCH --output=%x_%j.out
 
 module load anaconda
 
